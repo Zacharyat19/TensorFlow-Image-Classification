8024f5cfd85da4ba1d290c90759ab0dbc068cc5f
Alex-x90 <sashanrosenthal@gmail.com>
2019-07-16 10:37:48 -0700

    Added filepaths to main, added requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Conventional Neural network layering
 
 #Import libraries and packages
+import keras
 from keras.models import Sequential
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
@@ -10,18 +11,13 @@ from keras.preprocessing.image import ImageDataGenerator
 from IPython.display import display
 from PIL import Image
 
-#Initialize model
-model = Sequential()
 
-#Pooling
-model.add(Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'))
-
-#Flattening
-model.add(Flatten())
-
-#Full connection
-model.add(Dense(output_dim = 128, activation = 'relu'))
-model.add(Dense(output_dim = 1, activation = 'sigmoid'))
+model = keras.Sequential([
+    keras.layers.Convolution2D(32, 3, 3, input_shape = (64, 64, 3), activation = 'relu'),
+    keras.layers.Flatten(),
+    keras.layers.Dense(output_dim = 128, activation = 'relu'),
+    keras.layers.Dense(output_dim = 1, activation = 'sigmoid')
+])
 
 #Compiler
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
@@ -37,14 +33,14 @@ trainDatagen = ImageDataGenerator(
 testDatagen = ImageDataGenerator(rescale = 1./255)
 
 trainingSet = trainDatagen.flow_from_directory(
-    'train data',
+    'GTSRB_Final_Training_Images/GTSRB/Final_Training/Images',
     targetSize = (64, 64),
     batchSize = 32,
     classMode = 'binary'
 )
 
 testSet = testDatagen.flow_from_directory(
-    'test data',
+    'GTSRB_Final_Test_Images/GTSRB/Final_Test/Images',
     targetSize(64, 64),
     batchSize = 32,
     classMode = 'binary'
@@ -53,7 +49,7 @@ testSet = testDatagen.flow_from_directory(
 classifier.fit_generator(
     trainingSet,
     steps_per_epoch = 8000,
-    epochs = 10,
+    epochs = 1,
     validation_data = testSet,
     validation_steps = 800
 )
