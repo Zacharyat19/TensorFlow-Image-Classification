45cb62f5386a41b2837e3058fc5144c730afe736
Zach <Zacharyat19@gmail.com>
2019-07-17 10:08:29 -0700

    added documentaion.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Conventional Neural network layering
+#Using keras to create a conventional neural network through layering
 
 #Import libraries and packages
 import keras
@@ -10,18 +10,7 @@ from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 
-#checkpoint_path = "training_1/cp.ckpt"
-#checkpoint_dir = os.path.dirname(checkpoint_path)
-
-#model.load_weights(checkpoint_path)
-
-# Create checkpoint callback
-#cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
-
-#Compiler
-
-
-#Fitting images
+#Resizing images to 64 x 64
 trainDatagen = ImageDataGenerator(
     rescale = 1./255,
     shear_range = 0.2,
@@ -31,6 +20,7 @@ trainDatagen = ImageDataGenerator(
 
 testDatagen = ImageDataGenerator(rescale = 1./255)
 
+#link data directory
 trainingSet = trainDatagen.flow_from_directory(
     'datasets/dogs-vs-cats/train',
     target_size = (64, 64),
@@ -43,7 +33,10 @@ testSet = testDatagen.flow_from_directory(
     class_mode = 'binary'
 )
 
+#Initialize Sequential model
 model = Sequential()
+#Start layering with Conv2D, starting at 32 layers
+#and inceasing by a factor of two
 model.add(Conv2D(64, (3,3), input_shape = (64, 64, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
@@ -52,7 +45,7 @@ model.add(Conv2D(64, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
-model.add(Conv2D(64, (3,3)))
+model.add(Conv2D(128, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
 
@@ -63,12 +56,15 @@ model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
+#Compiler
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
+#Set steps_per_epoch to dataset/batch_size
+#batch_size = 32 by default
 model.fit_generator(
     trainingSet,
-    steps_per_epoch = 50,
-    epochs = 50,
+    steps_per_epoch = 781,
+    epochs = 20,
     validation_data = testSet,
     validation_steps = 150,
     #callbacks = [cp_callback]
