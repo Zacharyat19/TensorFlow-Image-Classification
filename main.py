92def4f58c7c84fb45f8a767f5bb5513f0fd1fcf
Zach <Zacharyat19@gmail.com>
2019-07-17 09:21:31 -0700

    new module update.

--- main.py
+++ main.py
@@ -5,31 +5,21 @@ import keras
 import os
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from IPython.display import display
 from PIL import Image
 
-checkpoint_path = "training_1/cp.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
+#checkpoint_path = "training_1/cp.ckpt"
+#checkpoint_dir = os.path.dirname(checkpoint_path)
 
-model = keras.Sequential([
-    keras.layers.Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = "relu"),
-    keras.layers.Flatten(),
-    keras.layers.Dense(units = 128, activation = 'relu'),
-    keras.layers.Dense(units = 1, activation = 'sigmoid')
-])
-
-model.load_weights(checkpoint_path)
+#model.load_weights(checkpoint_path)
 
 # Create checkpoint callback
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
+#cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
 
 #Compiler
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
 
 #Fitting images
 trainDatagen = ImageDataGenerator(
@@ -42,22 +32,44 @@ trainDatagen = ImageDataGenerator(
 testDatagen = ImageDataGenerator(rescale = 1./255)
 
 trainingSet = trainDatagen.flow_from_directory(
-    'datasets/trafficSigns/GTSRB_Final_Training_Images/GTSRB/Final_Training/Images',
+    'datasets/dogs-vs-cats/train',
     target_size = (64, 64),
     class_mode = 'binary'
 )
 
 testSet = testDatagen.flow_from_directory(
-    'datasets/trafficSigns/GTSRB_Final_Test_Images/GTSRB/Final_Test/Images',
+    'datasets/dogs-vs-cats/test',
     target_size = (64, 64),
     class_mode = 'binary'
 )
 
+model = Sequential()
+model.add(Conv2D(64, (3,3), input_shape = (64, 64, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
 model.fit_generator(
     trainingSet,
-    steps_per_epoch = 10,
-    epochs = 20,
+    steps_per_epoch = 50,
+    epochs = 50,
     validation_data = testSet,
-    validation_steps = 80,
-    callbacks = [cp_callback]
+    validation_steps = 150,
+    #callbacks = [cp_callback]
 )
