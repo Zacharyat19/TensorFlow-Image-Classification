064d075d90fac1c58b3eed3788b9c0fe7f362d07
Zach <Zacharyat19@gmail.com>
2019-07-18 12:17:37 -0700

    update

--- main.py
+++ main.py
@@ -11,21 +11,34 @@ from keras.preprocessing.image import ImageDataGenerator
 from PIL import Image
 
 
-checkpoint_path = "training_1/cp.ckpt"
-checkpoint_dir = os.path.dirname(checkpoint_path)
+#checkpoint_path = "training_1/cp.ckpt"
+#checkpoint_dir = os.path.dirname(checkpoint_path)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
+#cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
 
 #Fitting images
-#Resizing images to 64 x 64
+#Resizing images
 trainDatagen = ImageDataGenerator(
-    rescale = 1./255,
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    rescale=1./255,
     shear_range = 0.2,
     zoom_range = 0.2,
-    horizontal_flip = True
+    horizontal_flip = True,
+    fill_mode = 'nearest'
 )
 
-testDatagen = ImageDataGenerator(rescale = 1./255)
+testDatagen = ImageDataGenerator(
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    rescale=1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest'
+)
 
 #link data directory
 trainingSet = trainDatagen.flow_from_directory(
@@ -44,24 +57,20 @@ testSet = testDatagen.flow_from_directory(
 model = Sequential()
 #Start layering with Conv2D, starting at 32 layers
 #and inceasing by a factor of two
-model.add(Conv2D(64, (3,3), input_shape = (64, 64, 3)))
-model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(64, 64, 3)))
 model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(64, (3,3)))
-model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.2))
 
-model.add(Conv2D(128, (3,3)))
-model.add(Activation('relu'))
+model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-
-model.add(Conv2D(256, (3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.2))
 
 model.add(Flatten())
-model.add(Dense(64, kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dense(128, kernel_regularizer=keras.regularizers.l2(0.001)))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
@@ -74,8 +83,9 @@ model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accu
 model.fit_generator(
     trainingSet,
     steps_per_epoch = 781,
-    epochs = 20,
+    epochs = 6,
     validation_data = testSet,
-    validation_steps = 10,
-    callbacks = [cp_callback]
+    validation_steps = 390,
+    #callbacks = [cp_callback]
 )
+model.save_weights('first_try.h5')
