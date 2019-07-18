c7fe652ab751361d713f525769beb07bda570b9e
Ben Hadad <ben@bsh.dev>
2019-07-18 12:26:58 -0700

    Improved accuracy.

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
 from PIL import Image
 
 
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1,save_best_only=True,mode="max")
 
 #Fitting images
 #Resizing images to 64 x 64
@@ -22,7 +23,7 @@ trainDatagen = ImageDataGenerator(
     rescale = 1./255,
     shear_range = 0.2,
     zoom_range = 0.2,
-    horizontal_flip = True
+    rotation_range = 15
 )
 
 testDatagen = ImageDataGenerator(rescale = 1./255)
@@ -47,23 +48,27 @@ model = Sequential()
 model.add(Conv2D(64, (3,3), input_shape = (64, 64, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.1))
 
 model.add(Conv2D(64, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.1))
 
 model.add(Conv2D(128, (3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
+model.add(Dropout(0.1))
 
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.1))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+sgd = optimizers.SGD(lr = 0.0001, decay = 0, momentum = 0.9, nesterov = True)
+model.compile(optimizer = sgd, loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 model.load_weights(checkpoint_path)
 
@@ -73,5 +78,9 @@ model.fit_generator(
     epochs = 20,
     validation_data = testSet,
     validation_steps = 10,
-    callbacks = [cp_callback]
+    callbacks = [cp_callback],
+    workers = 8,
+    max_queue_size = 25,
+    shuffle = True
 )
+
