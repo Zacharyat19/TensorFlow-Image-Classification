368fb833b8a45910653f6e113bb3541c37fa897f
Gavin Pham <44819801+GDPBoi@users.noreply.github.com>
2019-07-18 12:53:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Using keras to create a conventional neural network through layering
+#Conventional Neural network layering
 
 #Import libraries and packages
 import keras
@@ -8,39 +8,29 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
 from PIL import Image
-
 
 #checkpoint_path = "training_1/cp.ckpt"
 #checkpoint_dir = os.path.dirname(checkpoint_path)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1,save_best_only=True,mode="max")
+#model.load_weights(checkpoint_path)
+
+# Create checkpoint callback
+#cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,save_weights_only=True,verbose=1)
+
+#Compiler
+
 
 #Fitting images
-#Resizing images
 trainDatagen = ImageDataGenerator(
-    rotation_range = 40,
-    width_shift_range = 0.2,
-    height_shift_range = 0.2,
-    rescale=1./255,
+    rescale = 1./255,
     shear_range = 0.2,
     zoom_range = 0.2,
-    rotation_range = 15
+    horizontal_flip = True
 )
 
-testDatagen = ImageDataGenerator(
-    rotation_range = 40,
-    width_shift_range = 0.2,
-    height_shift_range = 0.2,
-    rescale=1./255,
-    shear_range = 0.2,
-    zoom_range = 0.2,
-    horizontal_flip = True,
-    fill_mode = 'nearest'
-)
+testDatagen = ImageDataGenerator(rescale = 1./255)
 
-#link data directory
 trainingSet = trainDatagen.flow_from_directory(
     'datasets/dogs-vs-cats/train',
     target_size = (64, 64),
@@ -53,42 +43,33 @@ testSet = testDatagen.flow_from_directory(
     class_mode = 'binary'
 )
 
-#Initialize Sequential model
 model = Sequential()
-#Start layering with Conv2D, starting at 32 layers
-#and inceasing by a factor of two
-model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(64, 64, 3)))
+model.add(Conv2D(64, (3,3), input_shape = (64, 64, 3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
 
-model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
 
-model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+model.add(Conv2D(64, (3,3)))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.1))
 
 model.add(Flatten())
-model.add(Dense(128, kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dense(64))
 model.add(Activation('relu'))
-model.add(Dropout(0.1))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
-sgd = optimizers.SGD(lr = 0.0001, decay = 0, momentum = 0.9, nesterov = True)
-model.compile(optimizer = sgd, loss = 'binary_crossentropy', metrics = ['accuracy'])
-
-# model.load_weights(checkpoint_path)
+model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 model.fit_generator(
     trainingSet,
-    steps_per_epoch = 781,
-    epochs = 6,
+    steps_per_epoch = 50,
+    epochs = 50,
     validation_data = testSet,
-    validation_steps = 10,
-    callbacks = [cp_callback],
-    workers = 8,
-    max_queue_size = 25,
-    shuffle = True
+    validation_steps = 150,
+    #callbacks = [cp_callback]
 )
