5679ce1e5e99fb95e627c00a4f4a420262a4b583
Zach <Zacharyat19@gmail.com>
2019-07-19 09:35:16 -0700

    added graphing feature

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from PIL import Image
-
+from keras import optimizers
+import matplotlib.pyplot as plt
 
 checkpoint_path = "training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -24,10 +24,21 @@ trainDatagen = ImageDataGenerator(
     height_shift_range = 0.2,
     rescale=1./255,
     shear_range = 0.2,
-    zoom_range = 0.2
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest'
 )
 
-testDatagen = ImageDataGenerator(rescale = 1./255)
+testDatagen = ImageDataGenerator(
+    rotation_range = 40,
+    width_shift_range = 0.2,
+    height_shift_range = 0.2,
+    rescale=1./255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True,
+    fill_mode = 'nearest'
+)
 
 trainingSet = trainDatagen.flow_from_directory(
     'datasets/dogs-vs-cats/train',
@@ -55,9 +66,9 @@ model.add(MaxPooling2D(pool_size = (2, 2)))
 model.add(Dropout(0.1))
 
 model.add(Flatten())
-model.add(Dense(128, kernel_regularizer=keras.regularizers.l2(0.001)))
+model.add(Dense(128, kernel_regularizer=keras.regularizers.l2(0.01)))
 model.add(Activation('relu'))
-model.add(Dropout(0.1))
+model.add(Dropout(0.6))
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
@@ -66,13 +77,25 @@ model.compile(optimizer = sgd, loss = 'binary_crossentropy', metrics = ['accurac
 
 model.load_weights(checkpoint_path)
 
-model.fit_generator(
+history = model.fit_generator(
     trainingSet,
     steps_per_epoch = 781,
-    epochs = 20,
+    epochs = 10,
     validation_data = testSet,
-    validation_steps = 10,
+    validation_steps = 200,
     callbacks = [cp_callback],
     max_queue_size = 25,
     shuffle = True
 )
+
+#Graph of results
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
