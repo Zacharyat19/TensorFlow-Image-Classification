8e641a2cc1464af68bd7afb3998d4de78a0699cc
Alex-x90 <sashanrosenthal@gmail.com>
2019-08-01 14:05:24 -0700

    Moved GUI content to master.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# TenorFlow-Image-Classifiation
-An image classifier built using TensorFlow and the Keras API.
+# Signify
+An image classifier built using TensorFlow and Keras API.
+
+### Download:
+You can find a download for the application [here](https://www.mediafire.com/file/0ox1465h7wr7vrn/Signify.zip/file) and a web version [here](http://nwapw-tf.com/).
+
+If you wish to compile the application yourself, install pyinstaller and run ```pyinstaller -w -F -n "Signify" -i cats-dogs.ico main.py``` from the main directory. Once it finishes building the executable will be in the dist folder. Otherwise the python instructions are down below.
+
+### Requirements:
+Needs a h5 file with weights named model_weights.h5, a model file named model.json and a json file with the classes named class_names.json. Some premade models are located in the models folder. Drag the three required files into the same directory as either main.py (if you run the python file) or the "Signify" executable (if you compile it yourself). keras documentation on saving model to json and weights to h5 can be found [here](https://keras.io/getting-started/faq/#savingloading-only-a-models-architecture). You can generate the class_names json file with this code:
+```python
+import json
+
+class_names = json.dumps(TRAINING_GEN_NAME.class_indices)
+with open("class_names.json", "w") as json_file:
+    json_file.write(class_names)
+```
+If the code to generate class_names.json is unable to run, the format for class_names is a dict formatted like this: {"CLASS_0_NAME":0,"CLASS_1_NAME":1,"CLASS_2_NAME":2}
+
+### Directions:
+1. Make sure you have met the requirements mentioned above.
+2. Either select an image(.jpg, .png, .gif, .ppm and .ico files), or take a picture (press space in the take picture dialogue box).
+3. Enter the picture size that your model takes in (default is 64x64).
+4. Press the 'get prediction' button to get your prediction.
+
+### Python instructions:
+1. Make sure you are using python 3.6.8. The download is available [here](https://www.python.org/downloads/release/python-368/).
+2. Run ```pip install requirements.txt``` (if you wish to use your gpu for training with tensorflow, look at https://www.tensorflow.org/install/gpu)
+3. Make sure the requirements are fufilled.
+4. Run main.py either through idle or from cmd by navigating to it's directory and running ```python main.py```.
--- main.py
+++ main.py
@@ -1,71 +1,243 @@
-import os, keras
+import keras
+import os
 import tensorflow as tf
-from keras.models import Sequential
-from keras.layers import Conv2D, AveragePooling2D, Flatten, Dense, GaussianNoise, Activation
+from keras.models import Sequential,load_model,model_from_json
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
-from keras.losses import *
 from keras import optimizers
-from PIL import Image
 import matplotlib.pyplot as plt
-from tkinter.filedialog import asksaveasfilename
-from tkinter import Tk
-from keras.datasets import cifar10
-from keras.utils.np_utils import to_categorical
-def combined_loss(y_true, y_pred):
-    return mean_absolute_error(y_true, y_pred) * 4 + mean_squared_error(y_true, y_pred) * 4 + categorical_hinge(y_true, y_pred) * 2 + categorical_crossentropy(y_true, y_pred)
-trainDatagen = ImageDataGenerator(
-        rotation_range=15,
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        shear_range=0.2,
-        zoom_range=0.2,
-        channel_shift_range=0.1,
-        horizontal_flip=True,
-        vertical_flip=True,
-        validation_split=0.0
-)
-testDatagen = ImageDataGenerator()
-checkpoint = ModelCheckpoint("model_weights.h5", monitor='categorical_accuracy', verbose=1, save_best_only=True, mode='max')
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
-trainDatagen.fit(x_train)
-testDatagen.fit(x_test)
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(AveragePooling2D(pool_size = (2, 2)))
-model.add(GaussianNoise(0.1))
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(GaussianNoise(0.1))
-model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(GaussianNoise(0.1))
-model.add(Conv2D(256, (3, 3), activation='relu', padding='same'))
-model.add(Conv2D(256, (3, 3), activation='relu'))
-model.add(AveragePooling2D(pool_size = (2, 2)))
-model.add(GaussianNoise(0.1))
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(GaussianNoise(0.2))
-model.add(Dense(10, activation = 'softmax'))
-model.compile(optimizer = 'sgd', loss = combined_loss, metrics = ['categorical_accuracy'])
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-history = model.fit_generator(trainDatagen.flow(x_train, y_train, batch_size = 25), epochs = 200, steps_per_epoch = 1000, validation_data = testDatagen.flow(x_test, y_test, batch_size = 25), max_queue_size = 25, workers = 8, shuffle = True, callbacks = [checkpoint])
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-print(history.history.keys())
-plt.plot(history.history['categorical_accuracy'])
-plt.plot(history.history['val_categorical_accuracy'])
-plt.title('Model Accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch ID')
-plt.legend(['Training Set', 'Validation Set'], loc='upper left')
-plt.show()
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from tkinter.filedialog import askopenfilename
+from tkinter import *
+import tkinter as tk
+from PIL import Image, ImageTk
+from pathlib import Path
+import numpy as np
+import json
+import cv2
+from pandas import DataFrame
+import webbrowser
+from win32api import GetMonitorInfo, MonitorFromPoint
+
+guiHeight = 400
+guiWidth = 500
+picHeight = 256
+picWidth = 256
+infoPadding = 35
+defaultW = 64
+defaultH = 64
+backgroundColor="#505455"
+fontColor="white"
+picWinText="Take a picture"
+
+chart=None
+img=None
+loaded_model_json=None
+weightsDone=None
+class_names=None
+
+def callback(url):
+    webbrowser.open_new(url)
+
+class Lotfi(tk.Entry):
+    def __init__(self, master=None, **kwargs):
+        self.var = tk.StringVar()
+        tk.Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ''
+        self.var.trace('w', self.check)
+        self.get, self.set = self.var.get, self.var.set
+    def check(self, *args):
+        if self.get().isdigit():
+            # the current value is only digits; allow this
+            self.old_value = self.get()
+        else:
+            # there's non-digit characters in the input; reject this
+            self.set(self.old_value)
+
+def getPrediction():
+    global chart
+    global loaded_model_json
+    global weightsDone
+    global class_names
+    global model
+    if img is None:
+        messagebox.showinfo("Error","You must select an image first.")
+    else:
+        if loaded_model_json is None:
+            try:
+                infoLabel.config(text="Loading...")
+                master.update()
+                json_file = open("model.json","r")
+                loaded_model_json = json_file.read()
+                json_file.close()
+                model = model_from_json(loaded_model_json)
+            except:
+                messagebox.showinfo("Error","model.json wasn't found.")
+                infoLabel.config(text="Move model.json to the application directory.")
+                return
+        if weightsDone is None:
+            try:
+                model.load_weights("model_weights.h5")
+                weightsDone = True
+            except:
+                messagebox.showinfo("Error","model_weights.h5 wasn't found.")
+                infoLabel.config(text="Move model_weights.h5 to the application directory.")
+                return
+        if class_names is None:
+            try:
+                json_file = open("class_names.json","r")
+                class_names = json.loads(json_file.read())
+                json_file.close()
+                class_names = dict([[v,k] for k,v in class_names.items()])
+            except:
+                messagebox.showinfo("Error","class_names.json wasn't found.")
+                infoLabel.config(text="Move class_names.json to the application directory.")
+                return
+        inputWidth = int(wEntry.get())
+        inputHeight = int(hEntry.get())
+        imgPred = img.resize((inputWidth,inputHeight))
+        imgPred = np.array(imgPred)
+        imgPred = np.expand_dims(imgPred, axis=0)
+        try:
+            predictions = model.predict(imgPred)
+        except:
+            messagebox.showinfo("Error","Image size does not match model input.")
+            infoLabel.config(text="Enter the input size of your model in the height and width boxes.")
+            return
+        if chart is not None:
+            chart.pack_forget()
+        y_pred = [np.argmax(probas) for probas in predictions]
+        infoLabel.config(text="Predicted: "+class_names[y_pred[0]])
+        predictions=predictions[0]
+        top_values_index = np.argsort(predictions)[-3:]
+        top_values_index = top_values_index[::-1]
+        data = {"Class":[class_names[x] for x in top_values_index],"percentage":[predictions[x] for x in top_values_index]}
+        dataFr = DataFrame(data, columns= ['Class', 'percentage'])
+        dataFr = dataFr[['Class', 'percentage']].groupby('Class').sum()
+        figure = plt.Figure(figsize=(guiWidth/75,450/75), dpi=75)
+        ax = figure.add_subplot()
+        chart_type = FigureCanvasTkAgg(figure, frameGraph)
+        chart = chart_type.get_tk_widget()
+        chart.pack(side=BOTTOM)
+        dataFr.plot(kind='bar', ax=ax,rot=0)
+
+def loadImg():
+    global img
+    Tk().withdraw()
+    imgName = Path(askopenfilename(title = "Select file",filetypes = [("Picture files",("*.jpg","*.png","*.gif","*.ppm","*.ico"))]))
+    if str(imgName) is not ".":
+        try:
+            img = Image.open(imgName)
+            img = img.convert("RGB")
+        except:
+            messagebox.showinfo("Error","The image you tried to open was corrupted.")
+            return
+        displayImage()
+
+def displayImage():
+    global imgDisp
+    imgDisp = img.resize((picWidth,picHeight))
+    imgDisp = ImageTk.PhotoImage(imgDisp)
+    can.delete("all")
+    can.create_image((guiWidth-picWidth)/2,(guiHeight-picHeight)/2-infoPadding,image=imgDisp,anchor=NW)
+    can.create_rectangle((guiWidth-picWidth)/2-1,(guiHeight-picHeight)/2-1-infoPadding,guiWidth-((guiWidth-picWidth)/2),guiHeight-((guiHeight-picHeight)/2)-infoPadding)
+    infoLabel.config(text="Press get prediction.")
+
+def takePic():
+    global img
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow(picWinText,cv2.WINDOW_NORMAL)
+    cv2.moveWindow(picWinText,guiWidth,0)
+    tWinH = master.winfo_height()
+    temp,temp,cWinW,cWinH=cv2.getWindowImageRect(picWinText)
+    ratio = tWinH/cWinH
+    cv2.resizeWindow(picWinText,(int(ratio*cWinW),int(ratio*cWinH)))
+    infoLabel.config(text="Press space to take a picture.")
+    master.update()
+    temp=None
+    frame = None
+    while True:
+        ret, frame = cam.read()
+        try:
+            cv2.imshow(picWinText, frame)
+        except:
+            messagebox.showinfo("Error","You don't have a camera or your camera is disabled.")
+            return
+        if not ret:
+            break
+        k = cv2.waitKey(1)
+        if k%256 == 32:
+            break
+        if cv2.getWindowProperty(picWinText,1) == -1:
+            cam.release()
+            cv2.destroyAllWindows()
+            return
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    cam.release()
+    cv2.destroyAllWindows()
+    displayImage()
+
+master = Tk()
+master.geometry("+-8+0")
+monitor_info = GetMonitorInfo(MonitorFromPoint((0,0)))
+work_area = monitor_info.get("Work")
+master.maxsize(work_area[2], work_area[3]-35)
+master.configure(bg=backgroundColor)
+frame = Frame(master,bg=backgroundColor)
+frame.pack(side=RIGHT,fill=Y)
+scrollbar = Scrollbar(frame, orient=VERTICAL)
+listbox = Listbox(frame, yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
+listLabel = Label(frame,text="List of classes",bg=backgroundColor,fg=fontColor)
+listLabel.pack(side=TOP)
+scrollbar.pack(side=RIGHT, fill=Y)
+listbox.pack(side=LEFT, fill=BOTH, expand=1)
+try:
+    json_file = open("class_names.json","r")
+    class_names = json.loads(json_file.read())
+    json_file.close()
+    for x in class_names.keys():
+        listbox.insert(END,x)
+    class_names = dict([[v,k] for k,v in class_names.items()])
+except:
+    messagebox.showinfo("Error","class_names.json wasn't found.")
+infoLabel = Label(text="Select an image.",bg=backgroundColor,fg=fontColor,font=("Helvetica",15))
+infoLabel.pack(side=TOP,pady=(infoPadding,0))
+link = Label(text="Web version",bg=backgroundColor,fg="#00f9ff", cursor="hand2")
+link.pack(side=TOP)
+link.bind("<Button-1>", lambda e: callback("http://nwapw-tf.com/"))
+frameH = Frame(master)
+frameW = Frame(master,bg=backgroundColor)
+frameButtons = Frame(master)
+frameGraph = Frame(master)
+frameGraph.pack(side=BOTTOM)
+frameButtons.pack(side=BOTTOM)
+frameW.pack(side=BOTTOM)
+frameH.pack(side=BOTTOM)
+master.title("Image Identification")
+can = Canvas(master,width=guiWidth,height=guiHeight,bg=backgroundColor,highlightthickness=0)
+can.pack()
+can.create_rectangle((guiWidth-picWidth)/2,(guiHeight-picHeight)/2-infoPadding,guiWidth-((guiWidth-picWidth)/2),guiHeight-((guiHeight-picHeight)/2)-infoPadding,fill="grey")
+hLabel = Label(frameH,text="Input height:",bg=backgroundColor,fg=fontColor)
+hEntry = Lotfi(frameH)
+wLabel = Label(frameW,text="Input width:",bg=backgroundColor,fg=fontColor)
+wEntry = Lotfi(frameW)
+hLabel.pack(side=LEFT)
+hEntry.pack(side=LEFT)
+wLabel.pack(side=LEFT,padx=(5,0))
+wEntry.pack(side=LEFT)
+hEntry.delete(0, END)
+hEntry.insert(0, defaultH)
+wEntry.delete(0, END)
+wEntry.insert(0, defaultW)
+button1=tk.Button(frameButtons,text="Select a picture",command=loadImg,bg=backgroundColor,fg=fontColor)
+button2=tk.Button(frameButtons,text="Take a picture",command=takePic,bg=backgroundColor,fg=fontColor)
+button3=tk.Button(frameButtons,text="Get prediction",command=getPrediction,bg=backgroundColor,fg=fontColor)
+button4=tk.Button(frameButtons,text="Quit",command=master.quit,bg=backgroundColor,fg=fontColor)
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=LEFT)
+master.protocol("WM_DELETE_WINDOW",master.quit)
+mainloop()
